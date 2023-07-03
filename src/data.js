7951ef4b409edffb2960e963a37589d1d86945ae
Kgotso Makhalimele <kgotsom@protonmail.com>
2023-07-03 17:20:18 +0200

    added new destinations

--- src/data.js
+++ src/data.js
@@ -24,10 +24,32 @@ export default [
         id: 3,
         title: "Geirangerfjord",
         location: "Norway",
-        googleMapsUrl: "https://www.google.com/maps/place/Geirangerfjord/@62.1049045,6.9928129,12z/data=!3m1!4b1!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi?entry=ttu",
+        googleMapsUrl: "https://www.google.com/maps/place/Namaqualand/@-30.9954826,17.4328338,7z/data=!3m1!4b1!4m6!3m5!1s0x1c2e25be09eae75f:0x8de9a8227c2b9f1!8m2!3d-29.9786556!4d19.3783206!16zL20vMDQyM21q?entry=ttu",
         startDate: "03 January, 2023",
         endDate: "12 january, 2023",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    },
+
+    {
+        id: 4,
+        title: "Blyde River Canyon",
+        location: "South Africa",
+        googleMapsUrl: "https://www.google.com/maps/place/Blyde+River+Canyon/@-24.5911965,30.7953763,14z/data=!3m1!4b1!4m6!3m5!1s0x1ec256cdda7c57b1:0xb542746f8cfe522d!8m2!3d-24.591198!4d30.8128859!16zL20vMDY2eWJu?entry=ttu",
+        startDate: "14 April, 2023",
+        endDate: "20 April, 2023",
+        description: "The Blyde River Canyon is a 26km long Canyon located in Mpumalanga, South Africa. It is home to a large variety of wildlife, specifically South African species like the Vervet and Samango monkeys, Chacma baboons, Bush Babies, Black–backed jackals, Bush Pigs, Oribi, and the Grey Rhebuck.",
+        imageUrl: "https://media.tacdn.com/media/attractions-splice-spp-674x446/06/70/0d/c7.jpg"
+    },
+
+    {   
+        id: 5,
+        title: "Namaqualand",
+        location: "South Africa",
+        googleMapsUrl: "https://www.google.com/maps/place/Geirangerfjord/@62.1049045,6.9928129,12z/data=!3m1!4b1!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi?entry=ttu",
+        startDate: "24 September, 2023",
+        endDate: "4 October, 2023",
+        description: "Namaqualand is famous throughout the world for the spectacular sight of its many coloured wildflowers during the spring.  About 4,000 plant species grow in this area, and Namaqualand has more than 1,000 types of flowers that do not grow anywhere else in the world.",
+        imageUrl: "https://www.sa-venues.com/attractionsnc/gallery/namakwa/1b.jpg"
     }
 ]
